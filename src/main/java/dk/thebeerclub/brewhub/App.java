51245cf3a769efc6b04abfd0816833117d9c3bb8
Emil Madsen <emilmadsen@gmail.com>
2021-06-22 15:38:04 +0200

    rename app

--- src/main/java/dk/thebeerclub/brewhub/App.java
+++ src/main/java/dk/thebeerclub/brewhub/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BrewhubApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BrewhubApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
